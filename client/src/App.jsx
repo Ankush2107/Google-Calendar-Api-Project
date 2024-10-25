f14b7b939782984199d622b18fd92023e9dc0d3e
Ankush2107 <ankush.kumar210701@gmail.com>
2024-10-25 22:24:22 +0530

    added some code to set up google login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
+import dotenv from 'dotenv';
+dotenv.config();
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let clientId = process.env.CLIENT_ID
   return (
-    <>
+    <div>
+      <div className='App'>
+        <h1>Google Calender Api</h1>
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <GoogleLogin 
+          clientId={clientId} 
+          buttonText="Sign in & Authorise Calendar" 
+          onSuccess={responseGoogle}
+          onFailure={responseError}
+          cookiePolicy={'single_host_origin'}
+          // This is important
+          responseType='code'
+          accessType='offline'
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
